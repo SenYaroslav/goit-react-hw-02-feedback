6724c3319109a6342ec73379101aa38a3c4ea939
Iaroslav Seniuk <savuk7@icloud.com>
2022-12-04 08:43:30 +0200

    add start files

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Feedback;
