afd37a88bb2a72817b62978163a3e13fb3c82055
Iaroslav Seniuk <savuk7@icloud.com>
2022-12-05 11:51:40 +0200

    add some progress

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,14 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Feedback extends React.Component {
+class Feedback extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
+    total: 0,
+    positivePercentage: 0,
   };
 
+  onGoodBtnClick = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+
+  onNeutralBtnClick = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+
+  onBadBtnClick = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+
+  // countTotalFeedback = () => {
+  //   this.setState({
+  //     total: this.state.good + this.state.neutral + this.state.bad,
+  //   });
+  // };
+
+  // (() => {
+  //   this.setState({
+  //     total: this.state.good + this.state.neutral + this.state.bad,
+  //   });
+  // })();
+
   render() {
-    return <div></div>;
+    const { good, neutral, bad, total, positivePercentage } = this.state;
+    return (
+      <>
+        <div className="feedback__container">
+          <h2 className="feedback__title">Please leave feedback</h2>
+          <button
+            className="feedback__btn"
+            type="button"
+            onClick={this.onGoodBtnClick}
+          >
+            Good
+          </button>
+          <button
+            className="feedback__btn"
+            type="button"
+            onClick={this.onNeutralBtnClick}
+          >
+            Neutral
+          </button>
+          <button
+            className="feedback__btn"
+            type="button"
+            onClick={this.onBadBtnClick}
+          >
+            Bad
+          </button>
+        </div>
+        <div className="statistics__container">
+          <h2 className="statistics__title">Statistics</h2>
+          <ul className="statistics__list">
+            <li className="statistics__item">Good: {good}</li>
+            <li className="statistics__item">Neutral: {neutral}</li>
+            <li className="statistics__item">Bad: {bad}</li>
+            <li className="statistics__item">Total: {total}</li>
+            <li className="statistics__item">
+              Positive feedback: {positivePercentage}
+            </li>
+          </ul>
+        </div>
+      </>
+    );
   }
 }
 
