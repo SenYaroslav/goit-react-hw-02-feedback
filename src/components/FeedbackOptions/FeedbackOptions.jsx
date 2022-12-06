cc784f87074dfae5e28f9048465f5b1e8175c2fc
Iaroslav Seniuk <savuk7@icloud.com>
2022-12-06 14:57:37 +0200

    add style files

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,9 +1,12 @@
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css'
+
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  return (
-    <div className="feedback__container">
+ return (
+    <div className={css.feedback__container}>
       {options.map(option => (
         <button
-          className="feedback__btn"
+          className={css.feedback__btn} 
           type="button"
           key={option}
           name={option}
@@ -17,3 +20,8 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 };
 
 export default FeedbackOptions;
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
